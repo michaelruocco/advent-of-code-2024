929d85a6b6ad70db322b93149940c538a3e6de27
Michael Ruocco <michael.ruocco@hotmail.com>
2024-12-31 12:11:48 +0000

    refactor so list of ids only has to be looped through once

--- src/main/java/uk/co/mruoc/day1/LocationIdsLoader.java
+++ src/main/java/uk/co/mruoc/day1/LocationIdsLoader.java
@@ -5,15 +5,16 @@ import uk.co.mruoc.file.FileLoader;
 
 public class LocationIdsLoader {
     public LocationIds load(String path) {
-        Collection<String> lines = FileLoader.loadContentLinesFromClasspath(path);
+        Collection<String[]> allParts = FileLoader.loadContentLinesFromClasspath(path).stream()
+                .map(line -> line.split("\\s+"))
+                .toList();
         return LocationIds.builder()
-                .one(lines.stream().map(line -> toId(line, 0)).toList())
-                .two(lines.stream().map(line -> toId(line, 1)).toList())
+                .one(allParts.stream().map(parts -> toId(parts, 0)).toList())
+                .two(allParts.stream().map(parts -> toId(parts, 1)).toList())
                 .build();
     }
 
-    private static Integer toId(String line, int index) {
-        String[] parts = line.split("\\s+");
+    private static Integer toId(String[] parts, int index) {
         return Integer.parseInt(parts[index]);
     }
 }
